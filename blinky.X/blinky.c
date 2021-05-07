ec6d4d2500d8193599a2882c105b7e86ef1fa76f
John Honniball <coredump@virginmedia.com>
2021-05-07 22:54:09 +0100

    Switch clock prescaler off so that MCU runs at 8MHz instead of 1MHz,

--- blinky.X/blinky.c
+++ blinky.X/blinky.c
@@ -23,8 +23,22 @@ static void dally(const int loops)
     }
 }
 
+
+/* initMCU --- set up the microcontroller in general */
+
+static void initMCU(void)
+{
+    // SAMD starts up at 1MHz, using 8MHz oscillator and divide-by-8 prescaler
+    
+    // Change prescaler to divide-by-1
+    SYSCTRL_REGS->SYSCTRL_OSC8M = (SYSCTRL_REGS->SYSCTRL_OSC8M & ~SYSCTRL_OSC8M_PRESC_Msk) | SYSCTRL_OSC8M_PRESC_0;
+}
+
+
 int main(void)
 {
+    initMCU();
+    
     PORT_REGS->GROUP[1].PORT_DIRSET = PORT_PB10;    // LED pin to output
     PORT_REGS->GROUP[0].PORT_DIRSET = PORT_PA28;    // Logic analyser pin to output
     
