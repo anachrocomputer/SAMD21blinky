0cefa587c188a1cc08e238401cb31e3a01ca75ef
John Honniball <coredump@virginmedia.com>
2021-05-08 21:21:06 +0100

    Configure GCLK0 for 48MHz CPU clock. Pick up DFLL calibration from NVM fuse registers.

--- blinky.X/blinky.c
+++ blinky.X/blinky.c
@@ -32,6 +32,34 @@ static void initMCU(void)
     
     // Change prescaler to divide-by-1
     SYSCTRL_REGS->SYSCTRL_OSC8M = (SYSCTRL_REGS->SYSCTRL_OSC8M & ~SYSCTRL_OSC8M_PRESC_Msk) | SYSCTRL_OSC8M_PRESC_0;
+    
+    // Set up one wait state for 48MHz
+    NVMCTRL_REGS->NVMCTRL_CTRLB = (NVMCTRL_REGS->NVMCTRL_CTRLB & ~NVMCTRL_CTRLB_RWS_Msk) | (1 << NVMCTRL_CTRLB_RWS_Pos);
+    
+    // Work around erratum
+    while ((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_DFLLRDY_Msk) == 0)
+        ;
+    
+    SYSCTRL_REGS->SYSCTRL_DFLLCTRL = SYSCTRL_DFLLCTRL_ENABLE(1);
+    
+    while ((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_DFLLRDY_Msk) == 0)
+        ;
+    
+    // Read calibration
+    uint32_t coarse = ((*(uint32_t*)(OTP4_ADDR + 4)) & 0xFC000000) >> 26;
+    uint32_t fine = (*(uint32_t*)(OTP4_ADDR + 8)) & 0x3FF;
+    
+    // Write calibration
+    SYSCTRL_REGS->SYSCTRL_DFLLVAL = SYSCTRL_DFLLVAL_COARSE(coarse) | SYSCTRL_DFLLVAL_FINE(fine); 
+    
+    while ((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_DFLLRDY_Msk) == 0)
+        ;
+    
+    // Select 48MHz DFLL clock
+    GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_IDC(1) | GCLK_GENCTRL_GENEN(1) | GCLK_GENCTRL_SRC(GCLK_GENCTRL_SRC_DFLL48M_Val) | GCLK_GENCTRL_ID(0);
+    
+    while (GCLK_REGS->GCLK_STATUS & GCLK_STATUS_SYNCBUSY_Msk)
+        ;
 }
 
 
